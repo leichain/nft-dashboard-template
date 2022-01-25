85848b1b78a084eeaaa7a9d4ae24fafaebb7ad14
leichain <87617429+leichain@users.noreply.github.com>
2022-01-25 08:53:59 -0800

    customize for crypto coven

--- src/config.js
+++ src/config.js
@@ -1,19 +1,19 @@
 export const CONFIG = {
   TEMPLATE: {
     // 1. Set your NFT collection contract address
-    "collection_address": "0x9498274b8c82b4a3127d67839f2127f2ae9753f4",
+    "collection_address": "0x5180db8F5c931aaE63c74266b211F580155ecac8",
 
     // 2. Set your blockchain chain ID where your NFT collection contract address is deployed (see below for value options)
-    "block_chain_id": "137",
+    "block_chain_id": "1",
 
     // 3. Set the default title of your dashboard. If found, this template uses the NFT Collection name for the title.
-    "title": "My NFT Collection",
+    "title": "Crypto Coven Collection",
 
     // 4. (Optional) Display the floor price chart
     "timeseries_chart": true,
 
     // 5. (Optional) Set your banner image
-    "banner_picture": "https://www.superflexfitness.com/wp-content/uploads/2017/03/3D-banner-background.jpg",
+    "banner_picture": "https://lh3.googleusercontent.com/_0TJWleaEBb3-dywprgutJ2nEqZaWW3dHpVSHnki9cmfBMxTQNW9QNkRa8bdWpdzH1H0nVRRCMnAjiBCnSC2Axh7Dd-6UyqxhFQJLKI=w600",
   },
 
   FILTER_OPTIONS : [
